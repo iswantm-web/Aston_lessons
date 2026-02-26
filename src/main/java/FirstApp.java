c334e4da4a4e9eb63ed2d357044c7efb41440d91
Svetlana Lebedeva <i.swan.tm@gmail.com>
2026-02-26 16:52:51 +0700

    test2

---
+++ src/main/java/FirstApp.java
@@ -0,0 +1,10 @@
+public class FirstApp {
+    public static void main(String[] args) {
+        String a = "Banana";
+        String b = "Orange";
+        String c = "Apple";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
